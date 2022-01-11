e57c40d2cca6d17807e00e1fcd313587079a524e
Bartlomiej Swies <bartlomiejswies@gmail.com>
2022-01-11 14:12:25 +0100

    Code organizing

---
+++ firstApp/app.js
@@ -0,0 +1,13 @@
+(function () {
+    'use strict';
+
+    angular.module('myFirstApp', []) // connecting to html view
+
+        .controller('MyFirstController', function ($scope) { // $scope - special variable for AngularJS
+            $scope.name = "SomeName";
+            $scope.sayHello = function () {
+            return "Hello AngularJS";
+            }
+        });
+ 
+})();
