c66a9d390337e804418c63e04dbc8fb157388408
Bartlomiej Swies <bartlomiejswies@gmail.com>
2022-01-16 00:52:10 +0100

    Other ways for watchers

--- 08 - digestCycle/app.js
+++ 08 - digestCycle/app.js
@@ -9,6 +9,7 @@
     function CounterController ($scope) {
         $scope.onceCounter = 0;
         $scope.counter = 0;
+        $scope.name = "Somename";
 
         $scope.showNumberOfWatchers = function() {
             // $$ - element of scope
@@ -21,17 +22,23 @@
 
         $scope.upCounter = function() {
             $scope.counter++;
-        }
+        };
+
+        $scope.$watch(function() {
+            console.log("Digest Loop Fired!");
+        });
 
         // $watch - scope's function that check if data is changing - here onceCounter
-        $scope.$watch('onceCounter', function(newValue, oldValue) {
-            console.log("onceCounter old value: ", oldValue);
-            console.log("onceCounter new value: ", newValue);
-        });
+        // $scope.$watch('onceCounter', function(newValue, oldValue) {
+        //     console.log("onceCounter old value: ", oldValue);
+        //     console.log("onceCounter new value: ", newValue);
+        // });
 
-        $scope.$watch('counter', function(newValue, oldValue) {
-            console.log("counter old value: ", oldValue);
-            console.log("counter new value: ", newValue);
-        });
+        // $scope.$watch('counter', function(newValue, oldValue) {
+        //     console.log("counter old value: ", oldValue);
+        //     console.log("counter new value: ", newValue);
+        // });
+
+
     }
 })();
